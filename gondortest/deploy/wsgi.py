bccd9356a8f2cfdadec1f4d7227a87e05d8297a9
Byron <byroncorrales@gmail.com>
2011-08-07 21:02:07 -0600

    last changes

--- gondortest/deploy/wsgi.py
+++ gondortest/deploy/wsgi.py
@@ -1,5 +1,5 @@
 import os, sys
 
 from django.core.handlers.wsgi import WSGIHandler
-os.environ["DJANGO_SETTINGS_MODULE"] = "myproject.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "gondortest.settings"
 application = WSGIHandler()
